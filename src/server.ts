c0eb9203cfbf65af57e3c2e74ac7c32e90092100
mehnaz <mehnazchowdhury613@gmail.com>
2024-05-22 20:20:11 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import app from "./app";
 
 import mongoose from "mongoose";
 import config from "./config";
+import { Response } from "express";
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+
+    app.get("/", (req, res: Response) => {
+      res.send({ message: "Welcome to Assignment-2" });
+    });
 
     app.listen(config.port, () => {
       console.log(` app is listening on port ${config.port}`);
